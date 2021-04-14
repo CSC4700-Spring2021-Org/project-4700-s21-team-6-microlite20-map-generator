ecef0aa50ae9f89a3cdea569fe4543781d8b933c
kfsofronas <kfsofronas@gmail.com>
2021-04-13 21:05:00 -0400

    Update _template.java to use DungeonViewer

--- TabletopDungeonGenerator/src/main/_template.java
+++ TabletopDungeonGenerator/src/main/_template.java
@@ -19,7 +19,9 @@ public class _template {
 		//this is the test method, it prints out the random dungeon with a seed of 1234 at the default size
 		Dungeon dun = new Dungeon(1234);
 		dun.setLayout(randomize(dun.d, dun.SEED));
-		System.out.println(Arrays.toString(dun.d));
+//		System.out.println(Arrays.toString(dun.d));
+		DungeonViewer dv = new DungeonViewer(dun,10);
+		dv.setVisible(true);
 	}
 	
 }
